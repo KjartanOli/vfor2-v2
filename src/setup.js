26c2d8c3f825e803da04d3cf7e5b69e9757b696d
Kjartan Oli Agustsson <kjartanoli@disroot.org>
2024-02-17 11:53:28 +0000

    Use the provided DB schema

--- src/setup.js
+++ src/setup.js
@@ -5,7 +5,7 @@ async function main() {
 	const { teams, gamedays } = await parse_dir('./data');
 
 	for (const team of teams)
-		await query("INSERT INTO teams(name) VALUES($1);", [team]);
+		await query("INSERT INTO public.teams(name) VALUES($1);", [team]);
 
 	console.log();
 
@@ -13,9 +13,9 @@ async function main() {
 		const { date, games } = day;
 
 		for (const game of games) {
-			const { home_id, away_id } = (await query("SELECT t1.id as home_id, t2.id as away_id FROM teams t1, teams t2 WHERE t1.name = $1 AND t2.name = $2;", [game.home.name, game.away.name])).rows[0];
+			const { home, away } = (await query("SELECT t1.id as home, t2.id as away FROM public.teams t1, public.teams t2 WHERE t1.name = $1 AND t2.name = $2;", [game.home.name, game.away.name])).rows[0];
 
-			await query("INSERT INTO games(date, home, away, home_score, away_score) VALUES($1, $2, $3, $4, $5);", [date, home_id, away_id, game.home.score, game.away.score]);
+			await query("INSERT INTO public.games(date, home, away, home_score, away_score) VALUES($1, $2, $3, $4, $5);", [date, home, away, game.home.score, game.away.score]);
 		}
 	}
 }
