011fd52640b2db6d074e11e7657c7d2312cda979
Kjartan Oli Agustsson <kjartanoli@disroot.org>
2024-02-20 17:08:06 +0000

    Allow snake_case

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,7 @@ module.exports = {
     'import/prefer-default-export': 0,
     'import/no-extraneous-dependencies': 0,
     'no-param-reassign': 0,
+    'camelcase': 0, // CamelCase is both ugly and unreadable.
     quotes: ['error', 'single'],
     'no-console': [
       'warn',
